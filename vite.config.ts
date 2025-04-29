54d05313ecef690bcd5c7dc3dc781462d3a1a74b
Sanatani8502 <adityarajsingh.8502@gmail.com>
2025-04-30 02:13:52 +0530

    Fix vercel errorr

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    chunkSizeWarningLimit: 1600,
+  },
+  server: {
+    port: 3000,
+    host: true,
+  },
 })
